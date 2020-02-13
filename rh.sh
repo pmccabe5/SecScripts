a73e74e7eaf8f67cb4454535a3b2c8d20eed1bf7
Patrick McCabe <patrick.j.mccabe5@gmail.com>
2020-02-13 18:12:36 -0500

    Fixed Command

--- rh.sh
+++ rh.sh
@@ -11,7 +11,7 @@ sudo systemctl reload sshd.service
 echo "net.ipv4.conf.all.send_redirects=0" >> /etc/sysctl.conf
 echo "net.ipv4.conf.default.send_redirects=0" >> /etc/sysctl.conf
 echo "net.ipv4.conf.all.accept_redirects=0" >> /etc/sysctl.conf
-echo "net.ipv4.conf.all.accept_redirects=0" >> /etc/sysctl.conf
+echo "net.ipv4.conf.default.accept_redirects=0" >> /etc/sysctl.conf
 echo "net.ipv4.icmp.ignore_bogus_error_responses=1" >> /etc/sysctl.conf
 echo "net.ipv4.conf.default.accept_source_route=0" >> /etc/sysctl.conf
 echo "net.ipv4.conf.all.accept_source_route=0" >> /etc/sysctl.conf
@@ -20,7 +20,7 @@ echo "alias net-pf-4 off # IPX" >> /etc/modules.conf
 echo "alias net-pf-5 off # Appletalk" >> /etc/modules.conf
 echo "alias net-pf-10 off # IPv6" >> /etc/modules.conf
 echo "alias net-pf-12 off # Decnet" >> /etc/modules.conf 
-sudo yum install nmap epel-release -y
+sudo yum install nmap wireshark lynis clamav epel-release -y
 yum install fail2ban fail2ban-system -y
 yum update -y selinux-policy*
 sudo systemctl start fail2ban
