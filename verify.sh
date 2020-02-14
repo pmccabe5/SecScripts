9d4e0acf16f38f55b2c792e8b590e54dcf4c8d6b
lukem1 <37024616+lukem1@users.noreply.github.com>
2020-02-14 17:40:57 -0500

    Added md5 verification script

---
+++ verify.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Bash script to compare saved checksums with current checksums
+i=1
+while read line; do
+	IFS=' ' 
+	read sSum f <<< "$line"
+	read nSum nf <<< "$(md5sum $f)"
+	if [ "$sSum" != "$nSum" ]; then
+		echo $f Has Been Tampered With!
+	fi
+	i=$((i+1))
+
+done < sums.txt
