c2a567a7a0a0ab8a97528cefae1a8bb20a8198f1
Patrick McCabe <patrick.j.mccabe5@gmail.com>
2020-02-15 15:00:23 -0500

    Changed usermod command

--- rh.sh
+++ rh.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sysctl -w net.ipv6.conf.all.disable_ipv6=1
 sysctl -w net.ipv6.conf.default.disable_ipv6=1
-usermod -aG wheel $USER
+usermod -aG wheel $(whoami)
 yum remove garbagepackage -y
 echo "PermitRootLogin no" >> /etc/ssh/sshd_config
 echo "Protocol 2" >> /etc/ssh/sshd_config
