1d3181d53b66c7c2214f6af45e0013a4df5bfef2
Patrick McCabe <patrick.j.mccabe5@gmail.com>
2020-02-27 20:44:08 -0500

    Added Backup Script and cleaned up hardening script

--- rh.sh
+++ rh.sh
@@ -3,6 +3,7 @@ sysctl -w net.ipv6.conf.all.disable_ipv6=1
 sysctl -w net.ipv6.conf.default.disable_ipv6=1
 usermod -aG wheel $(whoami)
 yum remove garbagepackage -y
+yum install epel-release -y
 echo "PermitRootLogin no" >> /etc/ssh/sshd_config
 echo "Protocol 2" >> /etc/ssh/sshd_config
 echo "LogLevel VERBOSE" >> /etc/ssh/sshd_config
@@ -19,7 +20,7 @@ echo "alias net-pf-4 off # IPX" >> /etc/modules.conf
 echo "alias net-pf-5 off # Appletalk" >> /etc/modules.conf
 echo "alias net-pf-10 off # IPv6" >> /etc/modules.conf
 echo "alias net-pf-12 off # Decnet" >> /etc/modules.conf 
-sudo yum install net-tools nikto.noarch nmap wireshark lynis clamav.x86_64 epel-release -y
+sudo yum install net-tools nikto.noarch nmap wireshark lynis clamav.x86_64 -y
 yum install fail2ban fail2ban-system -y
 yum update -y selinux-policy*
 sudo systemctl start fail2ban
@@ -30,11 +31,13 @@ echo "port = 22" >> /etc/fail2ban/jail.d/sshd.local
 echo "logpath = /var/log/auth.log" >> /etc/fail2ban/jail.d/sshd.local
 echo "maxretry = 3" >> /etc/fail2ban/jail.d/sshd.local
 echo "bantime = 86400" >> /etc/fail2ban/jail.d/sshd.local
-sudo systemctl enable fail2ban
-sudo systemctl start fail2ban
-wget https://raw.github.com/emposha/Shell-Detector/master/shelldetect.py
-python shelldetect.py -r True -d ./
-echo "alias shells=\"python shelldetect.py -r True -d ./\"" >> ~/.bashrc
-exec bash
-git clone https://github.com/CISOfy/lynis
-cd lynis; ./lynis audit system >> vulnscan.txt
+sudo systemctl restart fail2ban
+#for the email server
+adduser pmccabe
+adduser dmeeder
+adduser spappas
+adduser mwalter
+adduser lmcguire
+adduser cmyers
+adduser bbillings
+adduser nkakadia
