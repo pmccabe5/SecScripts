fbeab172374005044b46036a2935a967db0a8f92
Patrick McCabe <patrick.j.mccabe5@gmail.com>
2020-04-02 10:37:05 -0400

    Changed RH Port

--- rh.sh
+++ rh.sh
@@ -4,6 +4,7 @@ sysctl -w net.ipv6.conf.default.disable_ipv6=1
 usermod -aG wheel $(whoami)
 yum remove garbagepackage -y
 yum install epel-release -y
+echo "#Port 4516" >> /etc/ssh/sshd_config
 echo "#PermitRootLogin no" >> /etc/ssh/sshd_config
 echo "#Protocol 2" >> /etc/ssh/sshd_config
 echo "#LogLevel VERBOSE" >> /etc/ssh/sshd_config
