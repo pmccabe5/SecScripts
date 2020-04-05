9f4e7b7898dd247ab2c2a14dbd95e23eb1c72eaa
Patrick McCabe <patrick.j.mccabe5@gmail.com>
2020-04-05 16:15:55 -0400

    Updated Backup Script

--- backup.sh
+++ backup.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 files="/etc/passwd /etc/shadow /etc/sudoers"
 user=$(whoami)
-dst="/root/backups"
+dst="/home/backups"
+mkdir $dst
 day=$(date +%Y-%m-%d)
 host=$(hostname -s)
 archive="$host-$day.tar.gz"
 echo "Backup has begun"
 tar czf $dst/$archive $files
 echo "Backup completed"
+cd $dst
+pwd $dst
 ls -alth $dst
